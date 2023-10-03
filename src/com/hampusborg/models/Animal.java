8ac13297741b89f16ac6c6806d4f593980ef4465
HampusCastle <Hampuscastle@gmail.com>
2023-10-03 11:44:40 +0200

    Second push

---
+++ src/com/hampusborg/models/Animal.java
@@ -0,0 +1,20 @@
+package com.hampusborg.abstractinterface;
+
+//Abstract och Inheritance (ARV)
+public interface Animal {
+
+    //TODO - Give relevant names
+    // Implement Inheritance
+    // Private keyword - vis/mod
+
+
+    //Abstract Method
+    //By default Methods are public
+    
+    void myAbstractMethod();
+
+
+    //Default??
+    default void test() {}
+    private void myTest() {}
+}
---
+++ src/com/hampusborg/models/Cat.java
@@ -0,0 +1,2 @@
+package com.hampusborg.models;public class Cat {
+}
